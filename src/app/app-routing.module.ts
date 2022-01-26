8b981859e932ae4cd4b4c3924935dfe97d1ea9af
giancarlo <giancarlosossam@gmail.com>
2022-01-26 10:09:00 -0500

    log-in 2.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
+
 const routes: Routes = [
-  
+  {path:'', redirectTo: 'inicio/sesion', pathMatch:'full'}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+ }
